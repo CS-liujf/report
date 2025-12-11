5875b9fb7582d5b3a087dc9530b3d6790db6adf3
fly <1756339062@qq.com>
2025-12-11 18:40:56 +0800

    perf: 将脚本改为立即执行函数

--- src/scriptTemplate.js
+++ src/scriptTemplate.js
@@ -1,19 +1,21 @@
-try {
+(async () => {
+  try {
     const seminarInfoArr = [];
     const postRequest = async (seminarInfo) => {
-        const paramJson = JSON.stringify(seminarInfo);
-        const formData = new URLSearchParams();
-        formData.append('paramJson', paramJson);
-        await fetch('https://graduate.shanghaitech.edu.cn/gsapp/sys/jzxxtjapp/tbgdj/save.do', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: formData
-        });
+      const paramJson = JSON.stringify(seminarInfo);
+      const formData = new URLSearchParams();
+      formData.append('paramJson', paramJson);
+      await fetch('https://graduate.shanghaitech.edu.cn/gsapp/sys/jzxxtjapp/tbgdj/save.do', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: formData
+      });
     };
     const postReqArr = seminarInfoArr.map((seminarInfo) => postRequest(seminarInfo));
-    Promise.all(postReqArr);
-} catch (e) {
+    await Promise.all(postReqArr);
+  } catch (e) {
     console.log(e);
-}
+  }
+})();

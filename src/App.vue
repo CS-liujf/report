2674db1e99e868e24aed82cc9a783d87e75ceabd
fly <1756339062@qq.com>
2025-12-12 10:25:58 +0800

    refactor: 将代码显示卡片封装成组件

--- src/App.vue
+++ src/App.vue
@@ -43,68 +43,8 @@
 
         <n-divider />
 
-        <!-- 代码显示区域 -->
-        <n-card>
-          <!-- 手动创建header -->
-          <n-flex justify="space-between" align="center" style="padding-bottom: 20px;">
-            <n-badge value="已过期" color="grey" :offset="[24, 8]" :show="false">
-              <div class="card-header">生成的代码</div>
-            </n-badge>
-
-            <n-popover placement="bottom" trigger="manual" ref="copyPopRef">
-              <template #trigger>
-                <n-button type="success" strong secondary size="small" @click="copyToClipboard"
-                  :disabled="codeContent.length === 0">
-                  <template #icon>
-                    <n-icon>
-                      <CopyIcon />
-                    </n-icon>
-                  </template>
-                  复制
-                </n-button>
-              </template>
-
-              <!-- 成功复制的提示 -->
-              <template v-if="copySuccess">
-                <n-flex align="center" :size="4">
-                  <n-icon color="#36AD6AFF" size="1.2rem">
-                    <SuccessIcon />
-                  </n-icon>
-                  <n-text style="font-size: 0.9rem">
-                    代码已复制到剪贴板，
-                    <n-button text tag="a"
-                      href="https://graduate.shanghaitech.edu.cn/gsapp/sys/yjsemaphome/portal/index.do" target="_blank"
-                      referrerpolicy="no-referrer" type="warning">
-                      立刻去用
-                    </n-button>
-                    <!-- 一定要设置为no-referrer，否则无法正常跳转 -->
-                  </n-text>
-                </n-flex>
-              </template>
-
-              <!-- 复制失败的提示 -->
-              <template v-else>
-                <n-flex align="center" :size="4">
-                  <n-icon color="#DE576DFF" size="1.2rem">
-                    <AlertIcon />
-                  </n-icon>
-                  <n-text style="font-size: 0.9rem">
-                    复制失败，请手动复制
-                  </n-text>
-                </n-flex>
-              </template>
-            </n-popover>
-          </n-flex>
-
-          <!-- 代码显示 -->
-          <template v-if="codeContent">
-            <n-code :code="codeContent" language="javascript" show-line-numbers word-wrap />
-          </template>
-
-          <template v-else>
-            <n-empty description="先输入学号和上传邮件" size="huge" />
-          </template>
-        </n-card>
+        <!-- 代码显示区 -->
+        <CodeCard :code="codeContent" :stale="false" />
 
 
         <template #footer>
@@ -124,9 +64,10 @@
 </template>
 
 <script setup lang="ts">
-import { ArchiveOutline as ArchiveIcon, Copy as CopyIcon, LogoGithub as GithubIcon, CheckmarkCircle as SuccessIcon, AlertCircle as AlertIcon } from '@vicons/ionicons5'
+import { ArchiveOutline as ArchiveIcon, LogoGithub as GithubIcon } from '@vicons/ionicons5'
 import NaiveProvider from '@/layout/NaiveProvider.vue';
 import ModeSwitch from '@/components/ModeSwitch/ModeSwitch.vue';
+import CodeCard from '@/components/CodeCard/CodeCard.vue';
 import { computed, ref, useTemplateRef } from 'vue';
 import { isDark } from './utils/switchMode';
 import { FormRules, ButtonProps, UploadFileInfo, FormValidationError } from 'naive-ui';
@@ -390,23 +331,6 @@ const handleSubmit = async () => {
 };
 
 
-const copyPopRef = useTemplateRef('copyPopRef')
-const copySuccess = ref(true);
-// 复制到剪贴板
-const copyToClipboard = async () => {
-  try {
-    await navigator.clipboard.writeText(codeContent.value);
-    copySuccess.value = true;
-  } catch (e) {
-    copySuccess.value = false;
-  }
-  finally {
-    copyPopRef.value?.setShow(true);
-    setTimeout(() => copyPopRef.value?.setShow(false), 3000);
-  };
-};
-
-
 const backgroundColor = computed(() => (isDark.value ? '#101014' : '#f6f9f8'));
 </script>
 
@@ -425,14 +349,5 @@ const backgroundColor = computed(() => (isDark.value ? '#101014' : '#f6f9f8'));
 .main-card {
   max-width: 880px;
   padding: 0rem 1.5rem;
-}
-
-.card-header {
-  font-size: 28px;
-  font-weight: var(--n-title-font-weight);
-  transition: color .3s var(--n-bezier);
-  flex: 1;
-  min-width: 0;
-  color: var(--n-title-text-color);
 }
 </style>
